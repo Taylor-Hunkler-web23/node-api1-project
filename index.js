b71d33ad31c482e3816c20a45cc8a168b0446258
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-28 18:46:07 -0400

    Created route handler for delete

--- index.js
+++ index.js
@@ -25,11 +25,15 @@ server.get('/api/users/:id', (req, res) => {
     const id = req.params.id;
     db.findById(id)
         .then(user => {
+            if (user){
             res.status(200).json(user);
+            }else{
+                res.status(404).json({message: "The user with the specified ID does not exist."})
+            }
         })
         .catch(err => {
             console.log('error', err);
-            res.status(404).json({  error: "The user with the specified ID does not exist." })
+            res.status(500).json({ error: "The users information could not be retrieved." })
         })
 })
 
@@ -37,22 +41,69 @@ server.get('/api/users/:id', (req, res) => {
 //post
 
 server.post('/api/users', (req, res) => {
-   
-    const {name, bio} =req.body;
+
+    const { name, bio } = req.body;
     if (!name || !bio) {
-        res.status(400).json({errorMessage: "Please provide name and bio for the user." })
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
     } else {
-     db.insert(req.body)
-      
-        .then(user=>{
-            res.status(201).json(user);
+        db.insert(req.body)
+
+            .then(user => {
+                res.status(201).json(user);
+            })
+            .catch(err => {
+                console.log('error', err);
+                res.status(500).json({ error: "There was an error while saving the user to the database" })
+            })
+    }
+})
+
+
+//delete
+server.delete('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    db.remove(id)
+
+        .then(removed => {
+            if (removed) {
+                res.status(200).json({ message: 'Deleted' });
+
+            } else {
+                res.status(404).json({ message: "The user with the specified ID does not exist." });
+            }
+
         })
         .catch(err => {
             console.log('error', err);
-            res.status(500).json({ error: "There was an error while saving the user to the database"  })
+            res.status(404).json({ error: "The user could not be removed" })
         })
-    }
 })
+
+//put
+server.put('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    const { name, bio } = req.body;
+
+    db.remove(id)
+    if (!name || !bio) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+    } else {
+        db.update(id, req.body)
+            .then(user => {
+                if (user) {
+                    res.status(200).json(user);
+                } else {
+                    res.status(404).json({ message: "The user with the specified ID does not exist." });
+                }
+
+            })
+            .catch(err => {
+                console.log('error', err);
+                res.status(404).json({ error: "The user could not be removed" })
+            })
+    }
+});
+
 
 const port = 5000;
 server.listen(port, () => console.log('\n=== API on port 5000 ===\n'));
