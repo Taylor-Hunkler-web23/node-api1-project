35722baadf7202a20164eb90e2e3461112adc144
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-28 15:35:03 -0400

    Created route handler for /api/users that returns array of users

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 // implement your API here
 const express = require('express');
+const db = require('./data/db.js')
 
-const server =express();
+const server = express();
 
+//get users
 
+server.get('/api/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.status(200).json(users);
+        })
+        .catch(err => {
+            console.log('error', err);
+            res.status(500).json({ error: "The users information could not be retrieved." })
+        })
+})
 
 
 const port = 5000;
