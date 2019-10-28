705ce6b0449bdc0bee47caa8b2633c1e20098641
Taylor <52665999+taylorhh5@users.noreply.github.com>
2019-10-28 15:54:19 -0400

    Created route handler for /users/id that returns user object with specified id

--- index.js
+++ index.js
@@ -17,6 +17,20 @@ server.get('/api/users', (req, res) => {
         })
 })
 
+//returns user with specified id
+
+server.get('/api/users/:id', (req, res) => {
+    const id = req.params.id;
+    db.findById(id)
+        .then(user => {
+            res.status(200).json(user);
+        })
+        .catch(err => {
+            console.log('error', err);
+            res.status(404).json({  error: "The user with the specified ID does not exist." })
+        })
+})
+
 
 const port = 5000;
 server.listen(port, () => console.log('\n=== API on port 5000 ===\n'));
